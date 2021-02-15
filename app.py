2f95a67f78998c81bf6d031dcad6a71c4f216ee5
Vicky Kumar <imvickykumar999@gmail.com>
2021-02-16 00:00:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+from flask import Flask, request, render_template, send_from_directory
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__)
+
+@app.route('/')
+def riet():
+    return render_template('riet.html')
+
+@app.route('/news')
+def news():
+
+    link = 'https://inshorts.com/en/read'
+    req = requests.get(link)
+
+    soup = bs(req.content, 'html5lib')
+    box = soup.findAll('div', attrs = {'class':'news-card z-depth-1'})
+
+    ha,ia,ba,la = [],[],[],[]
+    for i in range(len(box)):
+        h = box[i].find('span', attrs = {'itemprop':'headline'}).text
+
+        m = box[i].find('div', attrs = {'class':'news-card-image'})
+        m = m['style'].split("'")[1]
+
+        b = box[i].find('div', attrs = {'itemprop':'articleBody'}).text
+        l='link not found'
+
+        try:
+            l = box[i].find('a', attrs = {'class':'source'})['href']
+        except:
+            pass
+
+        ha.append(h)
+        ia.append(m)
+        ba.append(b)
+        la.append(l)
+    return render_template('news.html', ha=ha, ia=ia, ba=ba, la=la, len = len(ha))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
