b6581c5236b041842bcfbb70cb533c6fd9980d3a
Vicky Kumar <imvickykumar999@gmail.com>
2021-03-23 14:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,35 +18,35 @@ def riet():
     return is_occupied
 #     return render_template('riet.html')
 
-@app.route('/news')
-def news():
+# @app.route('/news')
+# def news():
 
-    link = 'https://inshorts.com/en/read'
-    req = requests.get(link)
+#     link = 'https://inshorts.com/en/read'
+#     req = requests.get(link)
 
-    soup = bs(req.content, 'html5lib')
-    box = soup.findAll('div', attrs = {'class':'news-card z-depth-1'})
+#     soup = bs(req.content, 'html5lib')
+#     box = soup.findAll('div', attrs = {'class':'news-card z-depth-1'})
 
-    ha,ia,ba,la = [],[],[],[]
-    for i in range(len(box)):
-        h = box[i].find('span', attrs = {'itemprop':'headline'}).text
+#     ha,ia,ba,la = [],[],[],[]
+#     for i in range(len(box)):
+#         h = box[i].find('span', attrs = {'itemprop':'headline'}).text
 
-        m = box[i].find('div', attrs = {'class':'news-card-image'})
-        m = m['style'].split("'")[1]
+#         m = box[i].find('div', attrs = {'class':'news-card-image'})
+#         m = m['style'].split("'")[1]
 
-        b = box[i].find('div', attrs = {'itemprop':'articleBody'}).text
-        l='link not found'
+#         b = box[i].find('div', attrs = {'itemprop':'articleBody'}).text
+#         l='link not found'
 
-        try:
-            l = box[i].find('a', attrs = {'class':'source'})['href']
-        except:
-            pass
+#         try:
+#             l = box[i].find('a', attrs = {'class':'source'})['href']
+#         except:
+#             pass
 
-        ha.append(h)
-        ia.append(m)
-        ba.append(b)
-        la.append(l)
-    return render_template('news.html', ha=ha, ia=ia, ba=ba, la=la, len = len(ha))
+#         ha.append(h)
+#         ia.append(m)
+#         ba.append(b)
+#         la.append(l)
+#     return render_template('news.html', ha=ha, ia=ia, ba=ba, la=la, len = len(ha))
 
 @app.errorhandler(404)
 def page_not_found(e):
